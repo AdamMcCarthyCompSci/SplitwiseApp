11fddccd10cf067209978ce185b4e5a2e97ad902
AdamMcCarthyCompSci <adam.mccarthy@ucdconnect.ie>
2021-09-13 13:42:41 +0200

    Test

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import GroupList from "./components/GroupList";
+import GroupPage from "./components/GroupPage";
+import ExpenseDetails from "./components/ExpenseDetails";
+import AddExpense from "./components/AddExpense";
+import { StyleSheet, View, FlatList } from 'react-native';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ApplicationProvider, IconRegistry, Layout, Text } from '@ui-kitten/components';
+import * as eva from '@eva-design/eva';
+import { default as defaultTheme } from './SplitwiseTheme.json';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { ThemeContext } from './ThemeContext';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [theme, setTheme] = React.useState('light');
+
+  const toggleTheme = () => {
+    const nextTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(nextTheme);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <IconRegistry icons={EvaIconsPack} />
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <ApplicationProvider {...eva} theme={{...eva[theme], ...defaultTheme}}>
+    <NavigationContainer>
+      <Stack.Navigator>
+            <Stack.Screen
+            name="GroupList"
+            component={GroupList}
+            />
+            <Stack.Screen
+            name="GroupPage"
+            component={GroupPage}
+            options={({route}) => ({title: route.params.title})}
+            />
+            <Stack.Screen 
+            name="ExpenseDetails" 
+            component={ExpenseDetails} 
+            options={({ route }) => ({title: route.params.title})}
+            />
+            <Stack.Screen
+            name="AddExpense"
+            component={AddExpense}
+            />
+    </Stack.Navigator>
+    </NavigationContainer>
+    </ApplicationProvider>
+    </ThemeContext.Provider>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  }
 });
